36953578936154b71bea0d21ec95de7439778bb7
Nick Stuhldreher <nickstuhldreher@Nicks-MBP-2.attlocal.net>
2023-04-05 12:58:08 -0400

    Test

---
+++ glee/glee.ino
@@ -0,0 +1,19 @@
+#include <MLX90393.h>
+#include <MLX90395.h>
+#include <MPU6000.h>
+#include <TMP117.h>
+#include <TPIS1385.h>
+#include <CAP.h>
+#include <LunaSat.h>
+#include <GLEE_Sensor.h>
+#include <GLEE_Radio.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
